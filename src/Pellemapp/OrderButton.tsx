4f527cc2ccfb0228204a8df3fc5f8b3804006aaa
kodpelle <pellewiklund@outlook.com>
2025-10-26 13:13:40 +0100

    feat: add StatusButton component for dynamic order status display

---
+++ src/Pellemapp/OrderButton.tsx
@@ -0,0 +1,10 @@
+import type { OrderStatus } from "./typer";
+import { buttonFor, buttonTextFor } from "./statusTheme";
+
+export function StatusButton({ status, onClick }: { status: OrderStatus; onClick: () => void; }) {
+    return (
+        <button className={`pt-3 pb-3 pr-3 ps-5 pe-5 ${buttonFor(status)}`} onClick={onClick} >
+            {buttonTextFor(status)}
+        </button>
+    );
+}
