54f52da31c3ec4461a45873d3db6544306d65413
kodpelle <pellewiklund@outlook.com>
2025-10-26 13:13:22 +0100

    refactor: simplify OrderHeader component by removing STATUS_STYLES and using iconFor for status icons

--- src/Pellemapp/OrderHeader.tsx
+++ src/Pellemapp/OrderHeader.tsx
@@ -1,19 +1,19 @@
-import { ShoppingCart } from "lucide-react";
 import type { OrderStatus } from "./typer";
+import { srufaceFor, iconFor } from "./statusTheme";
 
-const STATUS_STYLES: Record<OrderStatus, string> = {
-    NotStarted: "bg-danger text-white",
-    Started: "bg-warning text-black",
-    Finished: "bg-success text-white",
-} as const;
 
 export function OrderHeader({ orderNumber, status }: { orderNumber: string; status: OrderStatus; }) {
+    const Icon = iconFor(status);
+
     return (
-        <div className={`mt-5 mb-5 p-3 ${STATUS_STYLES[status]}`}>
-            <h1 className="text-3xl font-semibold">
-                Order number:&nbsp;<span className="fw-bold">#{orderNumber}</span>
-                <span><ShoppingCart className="me-1" /></span>
-            </h1>
+        <div className={`mt-5 mb-5 p-3 rounded ${srufaceFor(status)}`}>
+            <div className="">
+                <h1 className="d-flex flex-row  justify-content-between">
+                    Order number:&nbsp;#{orderNumber}
+                    <span className=""><Icon /></span>
+                </h1>
+
+            </div>
         </div>
     );
 }
