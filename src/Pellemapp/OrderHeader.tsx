de85595e0da397be823d2d434f92fd96a2a45a10
kodpelle <pellewiklund@outlook.com>
2025-10-27 22:30:02 +0100

    refactor: improve OrderHeader component structure and styling

--- src/Pellemapp/OrderHeader.tsx
+++ src/Pellemapp/OrderHeader.tsx
@@ -1,18 +1,33 @@
 import type { OrderStatus } from "./typer";
-import { srufaceFor, iconFor } from "./statusTheme";
+import { iconFor, surfaceFor } from "./statusTheme";
+
+type Props = {
+    orderNumber: string;
+    status: OrderStatus;
+    customerName?: string;
+    dateText: string;
+}
 
 
-export function OrderHeader({ orderNumber, status }: { orderNumber: string; status: OrderStatus; }) {
+
+export function OrderHeader({ orderNumber, status, customerName, dateText }: Props) {
     const Icon = iconFor(status);
 
     return (
-        <div className={`mt-5 mb-5 p-3 rounded ${srufaceFor(status)}`}>
-            <div className="">
-                <h1 className="d-flex flex-row  justify-content-between">
-                    Order number:&nbsp;#{orderNumber}
-                    <span className=""><Icon /></span>
-                </h1>
-
+        <div className="OrderHeader">
+            <div className={"p-3 mb-5 d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center gap-3"}>
+                <div className="d-flex align-items-center gap-3">
+                    <div>
+                        <h2 className="m-0 fw-semibold">Order: #{orderNumber}</h2>
+                        <div className="fw-s emibold">{dateText}</div>
+                        <div className="small opacity-75">{customerName}</div>
+                    </div>
+                </div>
+                <div className="text-md-end me-3">
+                    <div className={`icon-badge ${surfaceFor(status)}`}>
+                        <Icon />
+                    </div>
+                </div>
             </div>
         </div>
     );
