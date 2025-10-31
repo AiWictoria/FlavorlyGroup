4fb05b1002ee68fe75372fd1f731a59d8d386ddf
AmZu2024 <amar.zukanovic57@gmail.com>
2025-10-31 12:01:24 +0100

    initial backend setup for stripe. Will need some tweaks

---
+++ backend/RestRoutes/StripeRoutes.cs
@@ -0,0 +1,44 @@
+namespace RestRoutes;
+
+using Stripe.Checkout;
+using Microsoft.AspNetCore.Mvc;
+
+public static class StripeRoutes
+{
+  public static void MapStripeRoutes(this WebApplication app)
+  {
+    app.MapPost("api/stripe/create-checkout-session", async () =>
+    {
+      // Vi använder StripeConfiguration.ApiKey som vi sätter i Program.cs
+      var options = new SessionCreateOptions
+      {
+        PaymentMethodTypes = new List<string> { "card" },
+        Mode = "payment",
+        LineItems = new List<SessionLineItemOptions>
+            {
+                    new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            Currency = "sek",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = "Simulerad produkt"
+                            },
+                            UnitAmount = 49900 // 499 kr i öre
+                        },
+                        Quantity = 1
+                    }
+            },
+        //Vi kommer behöva ändra detta till själva sidorna i react appen när vi har dem
+        // SuccessUrl = "http://localhost:5173/success",
+        // CancelUrl = "http://localhost:5173/cancel"
+      };
+
+      var service = new SessionService();
+      var session = service.Create(options);
+
+      return Results.Json(new { id = session.Id }, statusCode: 200);
+    });
+  }
+}
