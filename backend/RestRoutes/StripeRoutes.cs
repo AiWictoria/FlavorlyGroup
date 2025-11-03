7914ee48839f42dc19d9302d53f2fb355431323b
AmZu2024 <amar.zukanovic57@gmail.com>
2025-11-03 14:01:33 +0100

    Activated SuccessUrl and CancelUrl for frontend redirection. Might change the actual redirections later for a better user-experience.
    Switched from returning session id to session url, which is required in the latest version of Stripe.js

--- backend/RestRoutes/StripeRoutes.cs
+++ backend/RestRoutes/StripeRoutes.cs
@@ -1,8 +1,9 @@
-namespace RestRoutes;
-
 using Stripe.Checkout;
 using Microsoft.AspNetCore.Mvc;
 
+namespace RestRoutes;
+
+// Egna notes:Kommer behöva skapa stripe session dynamiskt med produkterna från orchard
 public static class StripeRoutes
 {
   public static void MapStripeRoutes(this WebApplication app)
@@ -31,14 +32,14 @@ public static class StripeRoutes
                     }
             },
         //Vi kommer behöva ändra detta till själva sidorna i react appen när vi har dem
-        // SuccessUrl = "http://localhost:5173/success",
-        // CancelUrl = "http://localhost:5173/cancel"
+        SuccessUrl = "http://localhost:5173/order",
+        CancelUrl = "http://localhost:5173"
       };
 
       var service = new SessionService();
       var session = service.Create(options);
 
-      return Results.Json(new { id = session.Id }, statusCode: 200);
+      return Results.Json(new { url = session.Url }, statusCode: 200);
     });
   }
 }
