3a092fbcac12b38f4a25b3281c4d26f3929be85c
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-11-04 15:34:38 +0200

    Created a table from scratch using css. Is almost completely responsive on xl screens and mobile.

--- src/pages/ShoppingListPage.tsx
+++ src/pages/ShoppingListPage.tsx
@@ -1,5 +1,5 @@
 import { useShoppingList } from "../hooks/useShoppingList";
-import { Form, Button, Row, Col, Table } from "react-bootstrap";
+import { Form, Button, Row, Col, Table, Container } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import QuantitySelector from "../components/QuantitySelector";
 import Box from "../components/orderReceipt/Box.tsx";
@@ -59,7 +59,7 @@ export default function ShoppingListPage() {
           <h2>Shopping List</h2>
           <Form onSubmit={handleAdd}>
             <Row className="mt-4">
-              <Col xs={12} xl={4} className="mb-2">
+              <Col xs={12} xl={4}>
                 <Form.Group>
                   <IngredientSearch
                     onIngredientChange={(ingredient) =>
@@ -101,74 +101,45 @@ export default function ShoppingListPage() {
 
           {shoppingList.length > 0 ? (
             <>
-              <Table striped bordered hover>
-                <thead>
-                  <tr>
-                    <th>Ingredient</th>
-                    <th>Product</th>
-                    <th>Quantity</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {shoppingList.map((item) => (
-                    <tr key={item.id}>
-                      <td>
+              <Col className="shopping-list-container mb-5 p-1 m-1 fs-6">
+                {shoppingList.map((item, index) => (
+                  <>
+                    <Row
+                      key={index}
+                      className="shopping-list-item d-flex align-items-center p-1"
+                    >
+                      <Col xs={12} xl={3}>
+                        <span>
+                          <b>Ingredient:</b>{" "}
+                        </span>
                         {item.shoppingItemIngredient.title}{" "}
                         {item.shoppingItemIngredient.amount}{" "}
-                        {item.shoppingItemIngredient.unit.title}
-                      </td>
-                      <td>
+                        {item.shoppingItemIngredient.unit.title}{" "}
+                      </Col>
+                      <Col xs={12} xl={3} className="mt-1">
                         <Form.Select
                           size="sm"
                           value={product}
                           onChange={(e) => setProduct(e.target.value)}
                         >
-                          <option></option>
+                          <option value="">Choose product</option>
                           <option>Cherry Tomatoes 500g</option>
                           <option>Roma Tomatoes 1kg</option>
                         </Form.Select>
-                      </td>
-                      <td>
+                      </Col>
+                      <Col xs={12} xl={3} className="mt-1">
+                        <b>Product:</b> {product}
+                      </Col>
+                      <Col xs={6} xl={2}>
+                        <b>Total cost:</b>
+                      </Col>
+                      <Col xs={6} xl={1} className="d-flex justify-content-end">
                         <QuantitySelector value={1}></QuantitySelector>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-
-              <Row
-                className="mb-5 mt-5 p-1 m-1"
-                style={{
-                  backgroundColor: "#f2f2f2",
-                }}
-              >
-                {shoppingList.map((item) => (
-                  <>
-                    <Col xs={12} lg={4}>
-                      <span>
-                        <b>Ingredient:</b>{" "}
-                      </span>
-                      {item.shoppingItemIngredient.title}{" "}
-                      {item.shoppingItemIngredient.amount}{" "}
-                      {item.shoppingItemIngredient.unit.title}{" "}
-                    </Col>
-                    <Col xs={12} lg={4} className="mt-1">
-                      <Form.Select
-                        size="sm"
-                        value={product}
-                        onChange={(e) => setProduct(e.target.value)}
-                      >
-                        <option value="">Choose product</option>
-                        <option>Cherry Tomatoes 500g</option>
-                        <option>Roma Tomatoes 1kg</option>
-                      </Form.Select>
-                    </Col>
-                    <Col xs={12} lg={4} className="mt-1">
-                      <b>Product:</b> {product}
-                    </Col>
+                      </Col>
+                    </Row>
                   </>
                 ))}
-              </Row>
+              </Col>
 
               <div className="d-grid gap-3 mt-3 mb-4">
                 <Button>Add products to cart</Button>
