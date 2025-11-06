488adb4a63231bdb60f15fd21291d5d6edd9260e
kodpelle <pellewiklund@outlook.com>
2025-11-06 12:28:28 +0100

    fix: reformulation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -12,17 +12,17 @@ export default function HomePage() {
         <Row className="mx-1 text-center position-absolute top-50 start-50 translate-middle w-100">
           <Col>
             <h1 className="fs-1 text-light">Spara, Laga, Njut</h1>
-            <h3 className="fs-4 text-light">Hitta nya favoritrecept</h3>
+            <h3 className="fs-4 text-light">Hitta nya favoritrecept idag</h3>
             <div className="d-flex justify-content-center gap-3">
               <Button className="fs-6" variant="light" href="/recipes">
-                Se alla recept
+                Utforska recept
               </Button>
               <Button
                 className="fs-6"
                 variant="outline-light"
                 href="/createRecipe"
               >
-                Dela ett recept
+                Skapa recept
               </Button>
             </div>
           </Col>
