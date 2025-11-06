f4f4d32bf2bb66bd1b4e24e03547fad51911d6bf
AmZu2024 <amar.zukanovic57@gmail.com>
2025-11-06 12:34:00 +0100

    Changing language to swedish.

--- src/components/orderReceipt/Payment.tsx
+++ src/components/orderReceipt/Payment.tsx
@@ -1,35 +1,34 @@
-import { Col, Row, Alert } from "react-bootstrap";
+import { Col, Row, Alert, Button } from "react-bootstrap";
 import { useSearchParams } from "react-router-dom";
 import PayNowButton from "@orders/components/PayNowButton";
 interface PaymentProps {
   onNext: () => void;
   onBack: () => void;
 }
-export default function Payment({ onNext, onBack }: PaymentProps) {
+export default function Payment({ onBack }: PaymentProps) {
   const [searchParams] = useSearchParams();
   const status = searchParams.get("status");
   return (
     <>
+      <h2>Detta är betalning</h2>
       <Row className="mt-3 g-2 justify-content-center">
         <Col xs={10}>
           {status === "cancelled" && (
-            <Alert variant="warning">You cancelled the payment.</Alert>
+            <Alert variant="warning">Betalningen avbröts. Ingen debitering har skett. Försök igen!</Alert>
           )}
           {status === "failed" && (
-            <Alert variant="danger">The payment failed. Please try again.</Alert>
+            <Alert variant="danger">Betalningen misslyckades. Vänligen försök igen.</Alert>
           )}
         </Col>
 
         <Col xs={10}>
-          <h2>This is Payment</h2>
-          <button onClick={onNext}>Next</button>
-          <button onClick={onBack}>Back</button>
+          <Button variant="secondary" onClick={onBack}>Backa</Button>
         </Col>
       </Row>
 
       <Row className="mt-3 g-2 justify-content-center">
         <Col>
-          <PayNowButton />
+          <PayNowButton label="Återupta betalning" />
         </Col>
       </Row>
     </>
