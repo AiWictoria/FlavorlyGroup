48d1a1879ba41aa2815e9bf344c0709912d60256
kodpelle <pellewiklund@outlook.com>
2025-11-06 12:24:34 +0100

    Homepage now in swedish

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,18 +11,18 @@ export default function HomePage() {
 
         <Row className="mx-1 text-center position-absolute top-50 start-50 translate-middle w-100">
           <Col>
-            <h1 className="fs-1 text-light">Share, Cook, Enjoy</h1>
-            <h3 className="fs-4 text-light">Find new favorite recipes now</h3>
+            <h1 className="fs-1 text-light">Spara, Laga, Njut</h1>
+            <h3 className="fs-4 text-light">Hitta nya favoritrecept</h3>
             <div className="d-flex justify-content-center gap-3">
               <Button className="fs-6" variant="light" href="/recipes">
-                Browse Recipes
+                Se alla recept
               </Button>
               <Button
                 className="fs-6"
                 variant="outline-light"
                 href="/createRecipe"
               >
-                Share a Recipe
+                Dela ett recept
               </Button>
             </div>
           </Col>
