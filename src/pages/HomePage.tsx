5c76ec56a57b37527a77c028dcd9fa1828ac7866
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-11-07 11:57:04 +0200

    Solved issue where there was a vertical scrollbar on the home page. Changed height: 100vh to height: 100% and changed position absolute to fixed.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,26 +7,26 @@ export default function HomePage() {
   return (
     <>
       <div className="position-relative">
-        <div className="home-bg"></div>
-
-        <Row className="text-center position-absolute top-50 start-50 translate-middle w-100">
-          <Col>
-            <h1 className="fs-1 text-light">Share, Cook, Enjoy</h1>
-            <h3 className="fs-4 text-light">Find new favorite recipes now</h3>
-            <div className="d-flex justify-content-center gap-3">
-              <Button className="fs-6" variant="light" href="/recipes">
-                Browse Recipes
-              </Button>
-              <Button
-                className="fs-6"
-                variant="outline-light"
-                href="/createRecipe"
-              >
-                Share a Recipe
-              </Button>
-            </div>
-          </Col>
-        </Row>
+        <div className="home-bg">
+          <Row className="text-center position-absolute top-50 start-50 translate-middle w-100">
+            <Col>
+              <h1 className="fs-1 text-light">Share, Cook, Enjoy</h1>
+              <h3 className="fs-4 text-light">Find new favorite recipes now</h3>
+              <div className="d-flex justify-content-center gap-3">
+                <Button className="fs-6" variant="light" href="/recipes">
+                  Browse Recipes
+                </Button>
+                <Button
+                  className="fs-6"
+                  variant="outline-light"
+                  href="/createRecipe"
+                >
+                  Share a Recipe
+                </Button>
+              </div>
+            </Col>
+          </Row>
+        </div>
       </div>
     </>
   );
