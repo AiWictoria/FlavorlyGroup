c4387d12ccc6cebe5678c2f9276e11e82868f793
kodpelle <pellewiklund@outlook.com>
2025-11-07 10:01:17 +0100

    feat: update HomePage header text to English

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,9 +11,9 @@ export default function HomePage() {
 
         <Row className="mx-1 text-center position-absolute top-50 start-50 translate-middle w-100">
           <Col>
-            <h1 className="fs-1 text-light">Spara, Laga, Njut</h1>
-            <h3 className="fs-4 text-light">Hitta nya favoritrecept idag</h3>
-            <div className="d-flex justify-content-center gap-3">
+            <h1 className="fs-1 text-light">Share, Cook, Enjoy</h1>
+            <h3 className="fs-4 text-light mt-2">Hitta nya favoritrecept idag</h3>
+            <div className="d-flex justify-content-center gap-3 mt-4">
               <Button className="fs-6" variant="light" href="/recipes">
                 Utforska recept
               </Button>
