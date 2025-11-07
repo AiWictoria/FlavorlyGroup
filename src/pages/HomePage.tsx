759f2c67e1e59c4498029c01a06be6d257b109d8
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-11-07 15:21:13 +0200

    Added ms for Share, Cook, Enjoy for better layout.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
     <>
       <div className="position-relative">
         <div className="home-bg">
-          <Row className="text-center position-absolute top-50 start-50 translate-middle w-100">
+          <Row className="ms-1 text-center position-absolute top-50 start-50 translate-middle w-100">
             <Col>
               <h1 className="fs-1 text-light">Share, Cook, Enjoy</h1>
               <h3 className="fs-4 text-light mt-2">
