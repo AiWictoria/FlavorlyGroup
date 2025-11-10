bf73e0f91e773729f067c4b9fdac145d72d4ca82
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-11-10 09:14:41 +0200

    Fixed bug where you couldn't press the buttons on the homepage.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,30 +6,29 @@ HomePage.route = {
 export default function HomePage() {
   return (
     <>
-      <div className="position-relative">
-        <div className="home-bg">
-          <Row className="ms-1 text-center position-absolute top-50 start-50 translate-middle w-100">
-            <Col>
-              <h1 className="fs-1 text-light">Share, Cook, Enjoy</h1>
-              <h3 className="fs-4 text-light mt-2">
-                Hitta nya favoritrecept idag
-              </h3>
-              <div className="d-flex justify-content-center gap-3 mt-4">
-                <Button className="fs-6" variant="light" href="/recipes">
-                  Utforska recept
-                </Button>
-                <Button
-                  className="fs-6"
-                  variant="outline-light"
-                  href="/createRecipe"
-                >
-                  Skapa recept
-                </Button>
-              </div>
-            </Col>
-          </Row>
-        </div>
-      </div>
+      <div className="home-bg"></div>
+      <Row className="ms-1 text-center position-absolute top-50 start-50 translate-middle home-page-width">
+        <Col>
+          <h1 className="fs-1 text-light">Share, Cook, Enjoy</h1>
+          <h3 className="fs-4 text-light mt-2">Hitta nya favoritrecept idag</h3>
+          <div className="d-flex justify-content-center gap-3 mt-4">
+            <Button
+              className="fs-6 home-page-button "
+              variant="light"
+              href="/recipes"
+            >
+              Utforska recept
+            </Button>
+            <Button
+              className="fs-6 home-page-button"
+              variant="outline-light"
+              href="/createRecipe"
+            >
+              Skapa recept
+            </Button>
+          </div>
+        </Col>
+      </Row>
     </>
   );
 }
